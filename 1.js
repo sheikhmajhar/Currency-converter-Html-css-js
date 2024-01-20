6e6e96d8f82716f9426958b66ed4ad16785e4626
Sheikh Majhar <105147488+sheikhmajhar@users.noreply.github.com>
2024-01-20 14:00:28 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,245 @@
+
+const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+
+
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+const drop1 = document.querySelectorAll(".drop select")
+
+let button = document.querySelector("form button")
+let fromCurr = document.querySelector(".from select")
+let toCurr = document.querySelector(".to select")
+let msg = document.querySelector(".msg")
+
+
+
+window.addEventListener("load",()=>{
+    updateExchangeRate()
+})
+
+
+
+for (let num of drop1) {
+  for (currcode in countryList) {
+    let newOption = document.createElement("option")
+    newOption.innerText = currcode;
+    newOption.value = currcode;
+    num.append(newOption)
+
+
+
+
+
+    if (num.name === "from" && currcode === "USD") {
+      newOption.selected = "selected"
+    } else if (num.name === "to" && currcode === "INR") {
+      newOption.selected = "selected"
+    }
+  }
+
+  num.addEventListener("change", (evt) => {
+    updateFlag(evt.target)
+  })
+
+ 
+}
+
+const updateFlag = (element) => {
+  let currcode1 = element.value
+  let countryCode1 = countryList[currcode1]
+  let imgLink = `https://flagsapi.com/${countryCode1}/flat/64.png`
+  let img = element.parentElement.querySelector("img")
+  img.src = imgLink
+}
+
+
+button.addEventListener("click", async (evt) => {
+  evt.preventDefault()
+ updateExchangeRate()
+
+
+})
+
+const updateExchangeRate= async()=>{
+  let money = document.querySelector(".Money input")
+  let monval = money.value
+  if (monval === "" || monval < 1) {
+    monval = 1
+    monval.value = "1"
+  }
+
+  const url1 = `${url}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+
+  let response = await fetch(url1)
+
+  let data = await response.json()
+
+  let rate = data[toCurr.value.toLowerCase()]
+  console.log(rate)
+  let finalAmout = monval * rate;
+  msg.innerText = `${monval} ${fromCurr.value.toLowerCase()} = ${finalAmout} ${toCurr.value} `
+
+}
+
+
+
+
